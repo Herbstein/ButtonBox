fd0aaedc59137d00e0fe436350b851cb95686f61
Rasmus Larsen <doctorherbstein@gmail.com>
2017-04-21 08:53:41 +0200

    Added shift indicator

--- iRacingButtonBox/Program.cs
+++ iRacingButtonBox/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace iRacingButtonBox {
     using System;
+    using System.Diagnostics;
 
     using ArduinoDriver;
     using ArduinoDriver.SerialProtocol;
@@ -8,33 +9,75 @@
 
     using iRacingSdkWrapper;
 
-    internal class Program {
+    internal static class Program {
         private static ArduinoDriver driver;
+        private static TelemetryInfo Telemetry;
 
         private static void Main(string[] args) {
             var iracing = new SdkWrapper {
-                              TelemetryUpdateFrequency = 20
+                              TelemetryUpdateFrequency = 15
                           };
 
             iracing.TelemetryUpdated += OnTelemetryUpdated;
 
             driver = new ArduinoDriver(ArduinoModel.UnoR3, "COM4", true);
-            driver.Send(new PinModeRequest(2, PinMode.Output));
 
             Console.CursorVisible = false;
+
+            const byte DataPin = 12;
+            const byte ClockPin = 11;
+            const byte LatchPin = 10;
+
+            driver.Send(new PinModeRequest(DataPin, PinMode.Output));
+            driver.Send(new PinModeRequest(LatchPin, PinMode.Output));
+            driver.Send(new PinModeRequest(ClockPin, PinMode.Output));
+
+
+            driver.Send(new DigitalWriteRequest(LatchPin, DigitalValue.Low));
+            var response = driver.Send(new ShiftOutRequest(DataPin, ClockPin, BitOrder.LSBFIRST, 115));
+            driver.Send(new DigitalWriteRequest(LatchPin, DigitalValue.High));
+
+            Console.WriteLine($"{response.Value} = {Convert.ToString(response.Value, 2).PadLeft(8, '0')}");
 
             iracing.Start();
         }
 
-        private static void OnTelemetryUpdated(object sender, SdkWrapper.TelemetryUpdatedEventArgs e) { 
-            var clutch = e.TelemetryInfo.Clutch.Value;
-            var throttle = e.TelemetryInfo.Throttle.Value;
-            var brake = e.TelemetryInfo.Brake.Value;
+        private static void OnTelemetryUpdated(object sender, SdkWrapper.TelemetryUpdatedEventArgs e) {
+            var stopwatch = Stopwatch.StartNew();
+
+            const int IndicatorHalfWidth = 4;
+            var shftInd = e.TelemetryInfo.ShiftIndicatorPct.Value;
+            shftInd = shftInd.Remap(0, 1, 0, IndicatorHalfWidth);
 
             Console.SetCursorPosition(0, 0);
-            Console.WriteLine($"Clutch:\t\t{clutch:f5}");
-            Console.WriteLine($"Throttle:\t{throttle:f5}");
-            Console.WriteLine($"Brake:\t\t{brake:f5}");
+
+            Console.Write("Indicator:\t\t");
+
+            byte indicator = 0;
+            for (var i = 0; i < IndicatorHalfWidth*2; i++) {
+                indicator = (byte)(indicator << 1);
+                if (i < shftInd.RoundToInt() || i >= (IndicatorHalfWidth*2) - shftInd.RoundToInt()) {
+                    indicator |= 1;
+                }
+            }
+
+            Console.WriteLine(Convert.ToString(indicator, 2).PadLeft(8, '0'));
+
+            driver.Send(new DigitalWriteRequest(10, DigitalValue.Low));
+            driver.Send(new ShiftOutRequest(12, 11, BitOrder.MSBFIRST, indicator));
+            driver.Send(new DigitalWriteRequest(10, DigitalValue.High));
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Bitshift duration:\t{stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        public static float Remap(this float value, float from1, float to1, float from2, float to2) {
+            return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+        }
+
+        public static int RoundToInt(this float value) {
+            return (int)Math.Round(value);
         }
     }
 }
